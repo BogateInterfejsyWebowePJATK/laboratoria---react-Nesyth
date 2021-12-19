5a41fb397cc09a8c7ec2770067f603593c72f52f
Nesyth <n3syth@gmail.com>
2021-12-19 20:58:29 +0100

    init

---
+++ 10.2.1/Calculate.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+ 
+function Calculate() {
+  const [average, setAverage] = useState(0)
+  const [count, setCount] = useState(0)
+
+  function averageCount(v) {
+    setAverage(prevAverage => (prevAverage + v))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target.elements
+    setCount(prevCount => prevCount + 1)
+
+    averageCount(parseInt(value.value))
+  }
+
+  return (
+    <div className="calculate">
+      <form onSubmit={handleSubmit}>
+        <input type="number" id="value"/>
+        <button type="submit">Submit</button>
+      </form>
+      <span>submitted: {average} average: {average/count}</span>
+    </div>
+  );
+}
+
+export default Calculate;
